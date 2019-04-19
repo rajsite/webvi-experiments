0be4070476caa3aadb750891d3b1ef7a456852d3
rajsite <rajsite@users.noreply.github.com>
2019-04-18 22:31:29 -0500

    Initial migration of https://github.com/rajsite/jsli-prototype-examples

---
+++ AugmentedReality/AugmentedReality.gcomp/AugmentedReality.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    var arModel;
+
+    window.replaceUI = function (textSelector, jsAPI) {
+        var cb = jsAPI.getCompletionCallback();
+    
+        var textSelector = document.querySelector(textSelector);
+        textSelector.innerHTML = '';
+        var arFrame = document.createElement('iframe');
+        arFrame.style = 'width: 100%; height: 100%';
+        arFrame.src = 'AugmentedRealityFrame.html';
+        arFrame.addEventListener('load', function () {
+            arModel = arFrame.contentDocument.querySelector('a-gltf-model');
+            cb();
+        });
+        textSelector.appendChild(arFrame);
+    };
+
+    window.setModelHeight = function (newHeight) {
+        if (arModel !== undefined) {
+            arModel.object3D.position.y = newHeight;
+        }
+    };
+
+}());
