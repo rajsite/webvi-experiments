c7c5861153130b5e3c603e00c1352243edc9bbbf
rajsite <rajsite@users.noreply.github.com>
2019-04-24 03:24:14 -0500

    AugmentedReality Demo functional

--- AugmentedReality/AugmentedReality.gcomp/AugmentedReality.js
+++ AugmentedReality/AugmentedReality.gcomp/AugmentedReality.js
@@ -1,16 +1,17 @@
 (function () {
     'use strict';
 
-    const arModel;
+    var arModel;
 
     window.replaceUI = function (textSelector, jsAPI) {
         var cb = jsAPI.getCompletionCallback();
     
         var textSelector = document.querySelector(textSelector);
         textSelector.innerHTML = '';
+        textSelector.classList.add('augmented-reality-initialized');
         var arFrame = document.createElement('iframe');
         arFrame.style = 'width: 100%; height: 100%';
-        arFrame.src = 'AugmentedRealityFrame.html';
+        arFrame.src = 'AugmentedReality/AugmentedReality.html';
         arFrame.addEventListener('load', function () {
             arModel = arFrame.contentDocument.querySelector('a-gltf-model');
             cb();
