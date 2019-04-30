64e78a1f7161b5d73229bf7d713de5ba975c681f
rajsite <rajsite@users.noreply.github.com>
2019-04-29 21:18:40 -0500

    Implement Leaflet markers

--- Leaflet/Leaflet.gcomp/Leaflet.js
+++ Leaflet/Leaflet.gcomp/Leaflet.js
@@ -26,7 +26,7 @@
     }
     const refnumManager = new RefnumManager();
 
-    const createMap = function (selector) {
+    const mapCreate = function (selector, latitude, longitude, zoomLevel) {
         return new Promise(function (resolve) {
             const parents = document.querySelectorAll(selector);
             if (parents.length !== 1) {
@@ -39,32 +39,66 @@
             const element = document.createElement('div');
             element.style = 'width: 100%; height: 100%';
             parent.appendChild(element);
-            const austin = L.latLng(30.2672, -97.7431);
-            const map = L.map(element).setView(austin, 13);
+
+            const map = L.map(element).setView([latitude, longitude], zoomLevel);
             const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 // Must be attributed for OpenStreetMaps, see: https://www.openstreetmap.org/copyright
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
             });
 
             map.addLayer(tileLayer);
-            const refnum = refnumManager.createRefnum(map);
+            const mapRefnum = refnumManager.createRefnum(map);
             map.whenReady(function () {
-                resolve(refnum);
+                resolve(mapRefnum);
             });
         });
     };
 
-    const destroyMap = function (refnum) {
-        const map = refnumManager.getObject(refnum);
+    const mapDestroy = function (mapRefnum) {
+        const map = refnumManager.getObject(mapRefnum);
         if (map === undefined) {
             throw new Error('Invalid Leaflet map refnum');
         }
-        refnumManager.closeRefnum(refnum);
+        refnumManager.closeRefnum(mapRefnum);
         map.remove();
     };
 
+    const markerCreate = function (mapRefnum, latitude, longitude, text) {
+        const map = refnumManager.getObject(mapRefnum);
+        if (map === undefined) {
+            throw new Error('Invalid Leaflet map refnum');
+        }
+        const marker = L.marker([latitude, longitude]);
+        if (text.length !== 0) {
+            marker.bindPopup(text);
+        }
+        map.addLayer(marker);
+        const markerRefnum = refnumManager.createRefnum(marker);
+        return markerRefnum;
+    };
+
+    const markerPopupShow = function (markerRefnum) {
+        const marker = refnumManager.getObject(markerRefnum);
+        if (marker === undefined) {
+            throw new Error('Invalid Leaflet marker refnum');
+        }
+        marker.openPopup();
+    };
+
+    const markerDestroy = function (markerRefnum) {
+        const marker = refnumManager.getObject(markerRefnum);
+        if (marker === undefined) {
+            throw new Error('Invalid Leaflet marker refnum');
+        }
+        refnumManager.closeRefnum(markerRefnum);
+        marker.remove();
+    };
+
     window.WebVILeaflet = {
-        createMap,
-        destroyMap
+        mapCreate,
+        mapDestroy,
+        markerCreate,
+        markerDestroy,
+        markerPopupShow
     };
 }());
