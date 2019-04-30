8858beb508c5e585cfe29f1433ca9e9a130cea90
rajsite <rajsite@users.noreply.github.com>
2019-04-29 19:38:55 -0500

    Add leaflet whenReady callback check

--- Leaflet/Leaflet.gcomp/Leaflet.js
+++ Leaflet/Leaflet.gcomp/Leaflet.js
@@ -4,25 +4,30 @@
     'use strict';
 
     const createMap = function (selector) {
-        const parents = document.querySelectorAll(selector);
-        if (parents.length !== 1) {
-            throw new Error(`Expected to find one element with selector ${selector}. Instead found ${parents.length} elements`);
-        }
+        return new Promise(function (resolve) {
+            const parents = document.querySelectorAll(selector);
+            if (parents.length !== 1) {
+                throw new Error(`Expected to find one element with selector ${selector}. Instead found ${parents.length} elements`);
+            }
 
-        const parent = parents[0];
-        parent.innerHTML = '';
+            const parent = parents[0];
+            parent.innerHTML = '';
 
-        const element = document.createElement('div');
-        element.style = 'width: 100%; height: 100%';
-        parent.appendChild(element);
-        const austin = L.latLng(30.2672, -97.7431);
-        const map = L.map(element).setView(austin, 13);
-        const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            // Must be attributed for OpenStreetMaps, see: https://www.openstreetmap.org/copyright
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+            const element = document.createElement('div');
+            element.style = 'width: 100%; height: 100%';
+            parent.appendChild(element);
+            const austin = L.latLng(30.2672, -97.7431);
+            const map = L.map(element).setView(austin, 13);
+            const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                // Must be attributed for OpenStreetMaps, see: https://www.openstreetmap.org/copyright
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+            });
+
+            map.addLayer(tileLayer);
+            map.whenReady(function () {
+                resolve();
+            });
         });
-
-        map.addLayer(tileLayer);
     };
 
     window.WebVILeaflet = {
