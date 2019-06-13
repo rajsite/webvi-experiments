0f66276c17380dc73dc0a74370a9e536ea8f2afc
rajsite <rajsite@users.noreply.github.com>
2019-06-13 01:52:59 -0500

    DOM apis for getAttributes, createElement, appendChild, querySelector

--- DOM/DOM.gcomp/Support/DOM.js
+++ DOM/DOM.gcomp/Support/DOM.js
@@ -24,18 +24,28 @@
     }
     const referenceManager = new ReferenceManager();
 
-    const validateObject = function (obj, constructorFunction) {
-        if (obj instanceof constructorFunction === false) {
-            const name = constructorFunction.name;
-            const expectedNameMessage = (typeof name === 'string' && name !== '') ? `Expected a ${name} object instance.` : '';
+    const validateObject = function (obj, ...args) {
+        const constructorFunction = args.find(constructorFunction => obj instanceof constructorFunction);
+        if (constructorFunction === undefined) {
+            const names = args.map(constructorFunction => constructorFunction.name === '' ? 'UNKNOWN_NAME' : constructorFunction.name).join(',');
+            const expectedNameMessage = names === '' ? '' : ` Expected an instance of one of the following: ${names}.`;
             throw new Error('Invalid object.' + expectedNameMessage);
         }
     };
 
+    const appendChild = function (parentElementReference, elementOrDocumentFragmentReference) {
+        const parentElement = referenceManager.getObject(parentElementReference);
+        validateObject(parentElement, HTMLElement);
+        const elementOrDocumentFragment = referenceManager.getObject(elementOrDocumentFragmentReference);
+        validateObject(elementOrDocumentFragment, HTMLElement, DocumentFragment);
+        parentElement.appendChild(elementOrDocumentFragment);
+    };
+
+    // selectorsJSON: [selector]
     const createDocumentFragment = function (fragmentContent, selectorsJSON) {
         const template = document.createElement('template');
         template.innerHTML = fragmentContent;
-        const documentFragment = template.content.cloneNode(true);
+        const documentFragment = template.content;
         const selectors = JSON.parse(selectorsJSON);
         const elementReferences = selectors
             .map(selector => {
@@ -54,10 +64,18 @@
         });
     };
 
+    const createElement = function (tagName) {
+        var template = document.createElement('template');
+        template.content.appendChild(document.createElement(tagName));
+        var element = template.content.firstElementChild;
+        const elementReference = referenceManager.createReference(element);
+        return elementReference;
+    };
+
     // attributesJSON: [{name, value, remove}]
     const setAttributes = function (elementReference, attributesJSON) {
         const element = referenceManager.getObject(elementReference);
-        validateObject(HTMLElement, element);
+        validateObject(element, HTMLElement);
         const attributes = JSON.parse(attributesJSON);
         attributes.forEach(function ({name, value, remove}) {
             if (remove) {
@@ -68,20 +86,40 @@
         });
     };
 
+    // attributeConfigsJSON: [{name, default}]
+    // attributeResultsJSON: [{name, value, found}]
+    const getAttributes = function (elementReference, attributeConfigsJSON) {
+        const element = referenceManager.getObject(elementReference);
+        validateObject(element, HTMLElement);
+        const attributeConfigsInitial = JSON.parse(attributeConfigsJSON);
+        const attributeConfigs = attributeConfigsInitial.length === 0 ? element.getAttributes().map(name => ({name, default: ''})) : attributeConfigsInitial;
+        const attributeResults = attributeConfigs.map(function (attributeConfig) {
+            const valueInitial = element.getAttribute(attributeConfig.name);
+            const value = valueInitial === null ? attributeConfig.default : valueInitial;
+            const found = valueInitial === null ? false : true;
+            return {
+                name,
+                value,
+                found
+            }
+        });
+        const attributeResultsJSON = JSON.stringify(attributeResults);
+        return attributeResultsJSON;
+    };
+
     // TODO:
-    // getAttributes
     // getProperties
     // setProperties
-    // createElement
-    // appendChild
-    // querySelector
     // querySelectorAll
     // addEventListener
     // removeEventListener
     // classListAdd
     // classListRemove
     window.WebVIDOM = {
+        appendChild,
         createDocumentFragment,
-        setAttributes
+        createElement,
+        setAttributes,
+        getAttributes
     };
 }());
