bb5125e79bee010e20d313cfd277277d04c49f97
rajsite <rajsite@users.noreply.github.com>
2019-06-20 00:43:08 -0500

    Uppy implement deepMerge on options Makes it so that more options can be set from the gvi instead of needing custom logic in JS

--- Uppy/Uppy.gcomp/Support/Uppy.js
+++ Uppy/Uppy.gcomp/Support/Uppy.js
@@ -25,20 +25,49 @@
     }
     const referenceManager = new ReferenceManager();
 
+    const isObject = function (obj) {
+        return typeof obj === 'object' && obj !== null;
+    };
+
+    const deepMerge = function (target, ...args) {
+        args.forEach(function (arg) {
+            if (Array.isArray(arg)) {
+                if (!Array.isArray(target)) {
+                    throw new Error('Cannot merge an array onto a target non-array');
+                }
+                arg.forEach(item => target.push(item));
+            } else if (isObject(arg)) {
+                Object.keys(arg).forEach(function (prop) {
+                    if (Array.isArray(arg[prop])) {
+                        if (!Array.isArray(target[prop])) {
+                            target[prop] = [];
+                        }
+                        deepMerge(target[prop], arg[prop]);
+                    } else if (isObject(arg[prop])) {
+                        if (!isObject(target[prop])) {
+                            target[prop] = {};
+                        }
+                        deepMerge(target[prop], arg[prop]);
+                    } else {
+                        target[prop] = arg[prop];
+                    }
+                });
+            } else {
+                throw new Error('Cannot merge arg into target, arg must be an array or object to merge');
+            }
+        });
+    };
+
     const createUppy = function () {
         const uppyConfig = {
-            core: JSON.stringify({}),
-            dashboard: JSON.stringify({
-                locale: {
-                    strings: {
-                        uploadXFiles: {
-                            0: 'Open %{smart_count} file',
-                            1: 'Open %{smart_count} files'
-                        }
-                    }
-                }
-            }),
-            webcam: JSON.stringify({})
+            core: {
+                // Only one upload makes sense since the node blocks until upload finishes
+                allowMultipleUploads: false
+            },
+            dashboard: {
+                // don't want any element to trigger the dashboard
+                trigger: null
+            }
         };
 
         const reference = referenceManager.createReference(uppyConfig);
@@ -52,13 +81,14 @@
         }
     };
 
-    const setUppyOptions = function (reference, name, optionsJSON) {
+    const setUppyOptions = function (reference, optionsJSON) {
         const uppyConfig = referenceManager.getObject(reference);
         if (uppyConfig === undefined) {
             throw new Error('Invalid Uppy reference.');
         }
 
-        uppyConfig[name] = optionsJSON;
+        const options = JSON.parse(optionsJSON);
+        deepMerge(uppyConfig, options);
     };
 
     const requestUppyFiles = function (reference) {
@@ -69,20 +99,7 @@
             }
 
             let cleanup;
-            const core = JSON.parse(uppyConfig.core);
-            if (core.restrictions !== undefined) {
-                const keysToCoerce = ['maxFileSize', 'maxNumberOfFiles', 'minNumberOfFiles'];
-                keysToCoerce.forEach(function (key) {
-                    if (core.restrictions[key] === -1) {
-                        core.restrictions[key] = null;
-                    }
-                });
-
-                if (core.restrictions.allowedFileTypes.length === 0) {
-                    core.restrictions.allowedFileTypes = null;
-                }
-            }
-            core.onBeforeUpload = function (uppyFiles) {
+            uppyConfig.core.onBeforeUpload = function (uppyFiles) {
                 // Get the user selected file / blob
                 const uppyFileReferences = new Int32Array(Object.keys(uppyFiles)
                     .map(key => uppyFiles[key].data)
@@ -96,30 +113,24 @@
                 return false;
             };
 
-            // Only one upload makes sense since the node blocks until upload finishes
-            core.allowMultipleUploads = false;
-
-            const uppy = Uppy.Core(core);
+            const uppy = Uppy.Core(uppyConfig.core);
             cleanup = function () {
                 uppy.getPlugin('Dashboard').closeModal();
                 uppy.close();
             };
 
             const dashboardPlugins = [];
-            const webcam = JSON.parse(uppyConfig.webcam);
-            if (Object.keys(webcam).length > 0) {
+            if (uppyConfig.webcam !== undefined) {
                 dashboardPlugins.push('Webcam');
-                uppy.use(Uppy.Webcam, webcam);
+                uppy.use(Uppy.Webcam, uppyConfig.webcam);
             }
 
-            const dashboard = JSON.parse(uppyConfig.dashboard);
-            dashboard.plugins = dashboardPlugins;
-            dashboard.trigger = null;
-            dashboard.onRequestCloseModal = () => {
+            uppyConfig.dashboard.plugins = dashboardPlugins;
+            uppyConfig.dashboard.onRequestCloseModal = () => {
                 reject(new Error('No file selected'));
                 cleanup();
             };
-            uppy.use(Uppy.Dashboard, dashboard);
+            uppy.use(Uppy.Dashboard, uppyConfig.dashboard);
 
             // Open the dashboard and wait for selection or cancel
             uppy.getPlugin('Dashboard').openModal();
