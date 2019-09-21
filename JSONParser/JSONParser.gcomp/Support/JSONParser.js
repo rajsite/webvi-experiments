87f80e9ba967b55cb5f3345740938c066b635fe5
rajsite <rajsite@users.noreply.github.com>
2019-09-21 18:02:58 -0500

    JSONParser array of primitives optimization

--- JSONParser/JSONParser.gcomp/Support/JSONParser.js
+++ JSONParser/JSONParser.gcomp/Support/JSONParser.js
@@ -1,26 +1,45 @@
 (function () {
     'use strict';
-    // TokenTypes: <Array, ArrayEnd, Object, ObjectKey, ObjectEnd, Double, String, True, False, Null>
+    // TokenTypes: <Array, ArrayEnd, ArrayDouble, ArrayString, ArrayBoolean, Object, ObjectKey, ObjectEnd, Double, String, True, False, Null>
     // Token {Type, Data, Offsets}
     // Token {
-    //     T:  One of <A, AE, O, OK, OE, D, S, T, F, N>, field always included
+    //     T:  For all types -> One of <A, AE, AD, AS, AB, O, OK, OE, D, S, T, F, N>
     //     D:
-    //         For ObjectKey, simple string of key
-    //         For Double, simple string and because source is parsed with JSON.parse(), cannot be IEEE754 special tokens (NaN, Infinitiy, -Infinity)
-    //         For String, simple string
-    //         For other types, field ommitted
+    //         For ArrayDouble, ArrayString, ArrayBoolean -> JSON string of the corresponding types as a 1D array
+    //         For ObjectKey -> string of key
+    //         For Double -> string of number and cannot be IEEE754 special tokens (NaN, Infinitiy, -Infinity)
+    //         For String -> string
+    //         For other types -> field ommitted
     //     O:
-    //         For Array, offsets to each value and an additional offset to ArrayEnd
-    //         For Object, offsets to each ObjectKey (and the value starts one after the offset) and an additional offset to ObjectEnd
-    //         For other types, field omitted
+    //         For Array -> offsets to each value and an additional offset to ArrayEnd
+    //         For Object -> offsets to each ObjectKey (and the value starts one after the offset) and an additional offset to ObjectEnd
+    //         For other types -> field omitted
+    //         Note the offsets are relative to the current token not the top-level token array
     // }
-    // The Type field will always be emitted, the Data and Offsets will only be emitted as needed
-    // Examples:
+    // The Type field will always be emitted, the Data and Offsets will only be emitted as needed.
+    // Note the Types ArrayDouble, ArrayString, and ArrayBoolean do not have Offsets or corresponding ArrayEnd tokens.
+    //
+    // Example:
+    // {"hello": "world"} -> [{"T":"O","O":[1,3]}, {"T":"OK","D":"hello"}, {"T":"S","D":"world"}, {"T":"OE"}]
+    //
+    // Notable edge cases:
     // true -> [{"T":"T"}]
-    // {"hello": "world"} -> [{"T":"O","O":[1,3]},{"T":"OK","D":"hello"},{"T":"S","D":"world"},{"T":"OE"}]
+    // false -> [{"T":"F"}]
+    // null -> [{"T":"N"}]
+    // [] -> [{"T":"A","O":[1]}, {"T":"AE"}]
+    // {} -> [{"T":"O","O":[1]}, {"T":"OE"}]
+    // [1] -> [{"T":"AD","D":"[1]"}]
+    // [[1]] -> [{"T":"A","O":[1,2]}, {"T":"AD","D":"[1]"}, {"T":"AE"}]
+    // [[[1]]] -> [{"T":"A","O":[1,4]}, {"T":"A","O":[1,2]}, {"T":"AD","D":"[1]"}, {"T":"AE"}, {"T":"AE"}]
+    // [[1,2,3],[4,5,6],[7,8,9]] -> [{"T":"A","O":[1,2,3,4]}, {"T":"AD","D":"[1,2,3]"}, {"T":"AD","D":"[4,5,6]"}, {"T":"AD","D":"[7,8,9]"}, {"T":"AE"}]
+    // [1,null] -> [{"T":"A","O":[1,2,3]}, {"T":"D","D":"1"}, {"T":"N"}, {"T":"AE"}]
 
+    // TokenTypes
     const ARRAY = 'A';
     const ARRAYEND = 'AE';
+    const ARRAYDOUBLE = 'AD';
+    const ARRAYSTRING = 'AS';
+    const ARRAYBOOLEAN = 'AB';
     const OBJECT = 'O';
     const OBJECTKEY = 'OK';
     const OBJECTEND = 'OE';
@@ -30,10 +49,12 @@
     const FALSE = 'F';
     const NULL = 'N';
 
+    // Token Fields
     const TYPE = 'T';
     const DATA = 'D';
     const OFFSETS = 'O';
 
+    // Parsing functions
     let visitValue;
 
     const visitDouble = function (value) {
@@ -58,7 +79,41 @@
         return [{[TYPE]: NULL}];
     };
 
+    const visitArrayOfPrimitives = function (value) {
+        const tokens = [];
+        if (value.length < 1) {
+            return tokens;
+        }
+        const expectedType = typeof value[0];
+        if (expectedType !== 'number' && expectedType !== 'string' && expectedType !== 'boolean') {
+            return tokens;
+        }
+        for (let i = 0; i < value.length; i++) {
+            if (typeof value[i] !== expectedType) {
+                return tokens;
+            }
+        }
+        let tokenType;
+        if (expectedType === 'number') {
+            tokenType = ARRAYDOUBLE;
+        } else if (expectedType === 'string') {
+            tokenType = ARRAYSTRING;
+        } else {
+            tokenType = ARRAYBOOLEAN;
+        }
+        tokens.push({
+            [TYPE]: tokenType,
+            [DATA]: JSON.stringify(value)
+        });
+        return tokens;
+    };
+
     const visitArray = function (value) {
+        const arrayOfPrimitivesTokens = visitArrayOfPrimitives(value);
+        if (arrayOfPrimitivesTokens.length > 0) {
+            return arrayOfPrimitivesTokens;
+        }
+
         const tokens = [];
         const offsets = [];
 
@@ -133,5 +188,6 @@
         return tokensJSON;
     };
 
+    // Global property for JSLI
     window.WebVIJSONParser = {jsonTokenize};
 }());
