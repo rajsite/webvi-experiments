dc779336088a56533b57c8661ac64ff984c61287
rajsite <rajsite@users.noreply.github.com>
2019-10-07 03:05:42 -0500

    WebVICLI Express move jsli definitions into package

--- WebVICLI/packages/webvicli-express/src/index.js
+++ WebVICLI/packages/webvicli-express/src/index.js
@@ -1,12 +1,6 @@
 (function () {
     'use strict';
 
-    const express = require('express');
-    const createVireoMiddleware = require('./createVireoMiddleware');
-    const VireoMiddlewareRuntime = require('./VireoMiddlewareRuntime');
-    module.exports = {
-        express,
-        createVireoMiddleware,
-        VireoMiddlewareRuntime
-    };
+    const WebVIExpress = require('./WebVIExpress');
+    module.exports = {WebVIExpress};
 }());
