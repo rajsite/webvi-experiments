d6844e92cd25805168604e6066a11e1cabbed495
rajsite <rajsite@users.noreply.github.com>
2019-11-03 16:40:07 -0600

    RichText CKEditor first pass

---
+++ RichText/CKEditor.gcomp/Support/CKEditor.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    const create = async function (selector) {
+        const elements = document.querySelectorAll(selector);
+        if (elements.length !== 1) {
+            throw new Error(`Expected a single element with selector ${selector} but found ${elements.length}`);
+        }
+        const element = elements[0];
+        element.innerHTML = '';
+
+        const ckeditorContainer = document.createElement('div');
+        ckeditorContainer.classList.add('webvi-ckeditor-container');
+        const textarea = document.createElement('textarea');
+        ckeditorContainer.appendChild(textarea);
+        element.appendChild(ckeditorContainer);
+
+        await window.ClassicEditor.create(textarea);
+    };
+
+    window.WebVICKEditor = {create};
+}());
