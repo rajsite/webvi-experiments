9c565458e1c1f1f6dbf005f3d0c72cdc587c4a9a
rajsite <rajsite@users.noreply.github.com>
2019-11-09 18:27:09 -0600

    WebVIDebugger proof of concept nxg 5 support

--- WebVIDebugger/WebVIDebugger.gcomp/Support/WebVIDebugger.js
+++ WebVIDebugger/WebVIDebugger.gcomp/Support/WebVIDebugger.js
@@ -1,17 +1,67 @@
 (function () {
     'use strict';
+
+    const getReferences = function () {
+        const WebApplication = window.customElements.get('ni-web-application');
+        const webAppElement = document.querySelector('ni-web-application');
+
+        // nxg 3.1
+        if (window.vireoHelpers !== undefined) {
+            // Find vireo instance
+            const webApplicationModelsService = window.NationalInstruments.HtmlVI.webApplicationModelsService;
+            const webAppModel = webApplicationModelsService.getModel(webAppElement);
+            const updateService = webAppModel.updateService;
+            const vireo = updateService.vireo;
+
+            // Find vireoHelpers
+            const vireoHelpers = window.vireoHelpers;
+
+            return {
+                vireoHelpers,
+                vireo
+            };
+        }
+
+        // nxg 5? (tentative prototype, nxg 5 support not guaranteed, not in beta)
+        // Note: In nxg 4 we switched to es6 modules and these internals are no longer exposed in the global scope (good thing),
+        // but in order for the debug tools to be possible we might need to expose some properties (like the "ForDebugOnlyUnstable" properties below)
+        // TODO: Maybe we should just expose the vireo instance and helpers on the webapp element instead of all this exposing services stuff...
+        if (WebApplication.WebApplicationModelsServiceForDebugOnlyUnstable !== undefined) {
+            // Find vireo instance
+            const webApplicationModelsService = WebApplication.WebApplicationModelsServiceForDebugOnlyUnstable;
+            const webAppModel = webApplicationModelsService.getModel(webAppElement);
+            const updateService = webAppModel.updateService;
+            const vireo = updateService.vireo;
+
+            // Find vireoHelpers
+            const LocalUpdateService = updateService.constructor;
+            const vireoHelpers = LocalUpdateService.VireoHelpersForDebugOnlyUnstable;
+
+            return {
+                vireoHelpers,
+                vireo
+            };
+        }
+
+        // nxg 4 may be SOL, couldn't find any good ways to access internal state :(
+        return {
+            vireoHelpers: undefined,
+            vireo: undefined
+        };
+    };
+
     const inspectPanelValues = function (callChainJSON) {
         // static references
-        const encodeIdentifier = window.vireoHelpers.staticHelpers.encodeIdentifier;
+        const {vireoHelpers, vireo} = getReferences();
+        if (vireoHelpers === undefined || vireo === undefined) {
+            console.log('WebVIDebugger not supported in this version of NXG. Only');
+        }
         const jsonic = window.jsonic;
-
-        // instance references
-        const vireo = window.NationalInstruments.HtmlVI.webApplicationModelsService.getModel(document.querySelector('ni-web-application')).updateService.vireo;
 
         // input processing
         const callChain = JSON.parse(callChainJSON);
         const viName = callChain[callChain.length - 1];
-        const viNameEncoded = encodeIdentifier(viName);
+        const viNameEncoded = vireoHelpers.staticHelpers.encodeIdentifier(viName);
         const viRef = vireo.eggShell.findValueRef(viNameEncoded, '');
 
         // read dataspace
