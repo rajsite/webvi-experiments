77572bc8054f981a03675386d83e74e2290250e6
rajsite <rajsite@users.noreply.github.com>
2019-11-25 15:26:32 -0600

    Bootstrap npm dependencies

---
+++ Bootstrap/Bootstrap.gcomp/Support/Bootstrap.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    const loadBootstrap = function () {
+        // Intentionally empty
+        // Need a node to call so bootstrap dependencies are loaded
+    };
+
+    window.WebVIBootstrap = {loadBootstrap};
+}());
---
+++ Bootstrap/Navigation.gcomp/Support/Navigation.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    window.createNavbar = function () {
+
+    };
+
+}());
