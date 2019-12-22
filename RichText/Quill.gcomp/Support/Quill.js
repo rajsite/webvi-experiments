2ca20246e4a4806386321ca85ee090c5e56a902e
rajsite <rajsite@users.noreply.github.com>
2019-12-22 17:28:23 -0600

    RichText improve quill toolbar configurability

--- RichText/Quill.gcomp/Support/Quill.js
+++ RichText/Quill.gcomp/Support/Quill.js
@@ -1,4 +1,3 @@
-/* global Quill:false */
 (function () {
     'use strict';
 
@@ -25,40 +24,76 @@
     }
     const referenceManager = new ReferenceManager();
 
-    const quillTemplate = `
-        <div class="webvi-quill-container">
-            <div class="webvi-quill-toolbar">
-                <span class="ql-formats">
-                    <select class="ql-font"></select>
-                    <select class="ql-size"></select>
-                    <button class="ql-bold"></button>
-                    <button class="ql-italic"></button>
-                    <button class="ql-underline"></button>
-                    <button class="ql-strike"></button>
-                    <select class="ql-color"></select>
-                    <select class="ql-background"></select>
-                    <button class="ql-script" value="sub"></button>
-                    <button class="ql-script" value="super"></button>
-                    <button class="ql-header" value="1"></button>
-                    <button class="ql-header" value="2"></button>
-                    <button class="ql-blockquote"></button>
-                    <button class="ql-code-block"></button>
-                    <button class="ql-list" value="ordered"></button>
-                    <button class="ql-list" value="bullet"></button>
-                    <button class="ql-indent" value="-1"></button>
-                    <button class="ql-indent" value="+1"></button>
-                    <button class="ql-direction" value="rtl"></button>
-                    <select class="ql-align"></select>
-                    <button class="ql-link"></button>
-                    <button class="ql-image"></button>
-                    <button class="ql-video"></button>
-                    <button class="ql-formula"></button>
-                    <button class="ql-clean"></button>
-                </span>
+    const createQuillTemplate = function (formats) {
+        const quillTemplate = `
+            <div class="webvi-quill-container">
+                <div class="webvi-quill-toolbar">
+                    <span class="ql-formats">
+                    </span>
+                </div>
+                <div class="webvi-quill-editor"></div>
             </div>
-            <div class="webvi-quill-editor"></div>
-        </div>
-    `;
+        `;
+        // https://quilljs.com/docs/formats/
+        const quillButtonTemplates = {
+            // inline
+            'font': '<select class="ql-font"></select>',
+            'size': '<select class="ql-size"></select>',
+
+            'bold': '<button class="ql-bold"></button>',
+            'italic': '<button class="ql-italic"></button>',
+            'underline': '<button class="ql-underline"></button>',
+            'strike': '<button class="ql-strike"></button>',
+
+            'color': '<select class="ql-color"></select>',
+            'background': '<select class="ql-background"></select>',
+
+            'script': '<button class="ql-script" value="sub"></button><button class="ql-script" value="super"></button>',
+
+            'code': '<button class="ql-code"></button>',
+            'link': '<button class="ql-link"></button>',
+
+            // block
+            'header': '<button class="ql-header" value="1"></button><button class="ql-header" value="2"></button>',
+            'blockquote': '<button class="ql-blockquote"></button>',
+            'code-block': '<button class="ql-code-block"></button',
+
+            'list': '<button class="ql-list" value="ordered"></button><button class="ql-list" value="bullet"></button>',
+            'indent': '<button class="ql-indent" value="-1"></button><button class="ql-indent" value="+1"></button>',
+
+            'direction': '<button class="ql-direction" value="rtl"></button>',
+            'align': '<select class="ql-align"></select>',
+
+            // embeds
+            'formula': '<button class="ql-formula"></button>',
+            'image': '<button class="ql-image"></button>',
+            'video': '<button class="ql-video"></button>',
+
+            // actions
+            'clean': '<button class="ql-clean"></button>',
+
+            // custom
+            'spacer': '<div class="webvi-quill-spacer"></div>'
+        };
+        const template = document.createElement('template');
+        template.innerHTML = quillTemplate;
+        const templateClone = template.content.cloneNode(true);
+        const container = templateClone.querySelector('.webvi-quill-container');
+        const toolbar = templateClone.querySelector('.webvi-quill-toolbar');
+        const editor = templateClone.querySelector('.webvi-quill-editor');
+        const formatsContainer = templateClone.querySelector('.ql-formats');
+        formats.forEach(function (format) {
+            if (quillButtonTemplates[format] === undefined) {
+                throw new Error(`Unsupported format: ${format}. Supported formats: ${Object.keys(quillButtonTemplates).join(',')}`);
+            }
+            formatsContainer.insertAdjacentHTML('beforeend', quillButtonTemplates[format]);
+        });
+        return {
+            container,
+            toolbar,
+            editor
+        };
+    };
 
     const setDisabledHelper = function (quill, disabled) {
         const enable = !disabled;
@@ -66,61 +101,35 @@
         quill.options.bounds.classList.toggle('webvi-quill-disable', disabled);
     };
 
-    const removeHiddenToolbarFormats = function (webviQuillToolbar, toolbarFormats) {
-        const toolbarElements = [...webviQuillToolbar.querySelectorAll('.ql-formats > *')];
-        toolbarElements.forEach(element => {
-            const format = element.classList[0].substring('ql-'.length);
-            if (!toolbarFormats.includes(format)) {
-                element.parentNode.removeChild(element);
-            }
-        });
-    };
-
-    const createQuill = function (selector, formatsJSON, hiddenToolbarFormatsJSON, disabled) {
+    const createQuill = function (selector, formatsJSON, disabled) {
         const elements = document.querySelectorAll(selector);
         if (elements.length !== 1) {
             throw new Error(`Expected to find one element with selector ${selector}, but found ${elements.length}`);
         }
         const formats = JSON.parse(formatsJSON);
-        const hiddenToolbarFormats = JSON.parse(hiddenToolbarFormatsJSON);
-        const toolbarFormats = formats.filter(format => !hiddenToolbarFormats.includes(format));
-
-        const template = document.createElement('template');
-        template.innerHTML = quillTemplate;
-        const quillTemplateInstance = template.content;
-        const webviQuillContainer = quillTemplateInstance.querySelector('.webvi-quill-container');
-        const webviQuillToolbar = quillTemplateInstance.querySelector('.webvi-quill-toolbar');
-        const webviQuillEditor = quillTemplateInstance.querySelector('.webvi-quill-editor');
-        removeHiddenToolbarFormats(webviQuillToolbar, toolbarFormats);
-
         const element = elements[0];
         element.innerHTML = '';
-        element.appendChild(webviQuillContainer);
 
-        const quill = new Quill(webviQuillEditor, {
-            modules: {toolbar: webviQuillToolbar},
+        const {container, toolbar, editor} = createQuillTemplate(formats);
+        const filteredFormats = formats.filter(format => format !== 'spacer');
+        element.appendChild(container);
+
+        const config = {
+            modules: {toolbar},
             theme: 'snow',
-            bounds: webviQuillContainer,
-            formats
-        });
+            bounds: container,
+            formats: filteredFormats
+        };
+
+        const quill = new window.Quill(editor, config);
         setDisabledHelper(quill, disabled);
         const quillReference = referenceManager.createReference(quill);
-        window.quill = quill;
         return quillReference;
-
-        // var quill = new Quill('#editor-container', {
-        //     modules: {
-        //       syntax: true,
-        //       toolbar: '#toolbar-container'
-        //     },
-        //     placeholder: 'Compose an epic...',
-        //     theme: 'snow'
-        //   });
     };
 
     const destroyQuill = function (quillReference) {
         const quill = referenceManager.getObject(quillReference);
-        if (quill instanceof Quill === false) {
+        if (quill instanceof window.Quill === false) {
             return;
         }
         referenceManager.closeReference(quillReference);
@@ -129,7 +138,7 @@
 
     const getContents = function (quillReference) {
         const quill = referenceManager.getObject(quillReference);
-        if (quill instanceof Quill === false) {
+        if (quill instanceof window.Quill === false) {
             throw new Error('Expected instance of Quill object');
         }
         const deltas = quill.getContents();
@@ -139,7 +148,7 @@
 
     const setContents = function (quillReference, deltasJSON) {
         const quill = referenceManager.getObject(quillReference);
-        if (quill instanceof Quill === false) {
+        if (quill instanceof window.Quill === false) {
             throw new Error('Expected instance of Quill object');
         }
         const deltas = JSON.parse(deltasJSON);
@@ -148,7 +157,7 @@
 
     const setDisabled = function (quillReference, disabled) {
         const quill = referenceManager.getObject(quillReference);
-        if (quill instanceof Quill === false) {
+        if (quill instanceof window.Quill === false) {
             throw new Error('Expected instance of Quill object');
         }
         setDisabledHelper(quill, disabled);
