bec4b3c1f6e3f34d038ed73521716837952e0f83
rajsite <rajsite@users.noreply.github.com>
2019-12-28 00:57:01 -0600

    WebVINode Move Get Component Directory to NodePath

--- WebVINode/packages/@webvi-node/path/NodePath.gcomp/Support/NodePath.js
+++ WebVINode/packages/@webvi-node/path/NodePath.gcomp/Support/NodePath.js
@@ -9,5 +9,15 @@
         return absolutePath;
     };
 
-    module.exports = {resolve};
+    // This must be resolved from the Library component build output directory
+    // Assume path similar to Builds/ApplicationComponentRoot/NodePath/Support/NodePath.js
+    const componentDirectory = path.resolve(__dirname, '../../');
+    const getComponentDirectory = function () {
+        return componentDirectory;
+    };
+
+    module.exports = {
+        resolve,
+        getComponentDirectory
+    };
 }());
