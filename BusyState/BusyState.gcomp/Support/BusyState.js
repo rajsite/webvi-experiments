a7ae0c53a781dcf4fe5ae30e18fc14cb958dc0ac
rajsite <rajsite@users.noreply.github.com>
2020-01-28 14:00:31 -0600

    Lazy create busy state element

--- BusyState/BusyState.gcomp/Support/BusyState.js
+++ BusyState/BusyState.gcomp/Support/BusyState.js
@@ -1,16 +1,20 @@
 (function () {
     'use strict';
 
-    const busyElement = document.createElement('dialog');
-    // Prevent the dialog from being cancelled by pressing the escape key
-    busyElement.addEventListener('cancel', (evt) => evt.preventDefault());
-    busyElement.classList.add('busy-state');
-
-    if (window.dialogPolyfill !== undefined) {
-        window.dialogPolyfill.registerDialog(busyElement);
-    }
+    let busyElement;
 
     const setBusy = function () {
+        if (busyElement === undefined) {
+            busyElement = document.createElement('dialog');
+            // Prevent the dialog from being cancelled by pressing the escape key
+            busyElement.addEventListener('cancel', (evt) => evt.preventDefault());
+            busyElement.classList.add('busy-state');
+
+            if (window.dialogPolyfill !== undefined) {
+                window.dialogPolyfill.registerDialog(busyElement);
+            }
+        }
+
         if (document.body.contains(busyElement)) {
             throw new Error('Panel already in busy state, cannot set busy');
         }
