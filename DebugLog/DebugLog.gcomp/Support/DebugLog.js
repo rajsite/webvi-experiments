d72d786ca3e50f2540f2a4e31080666561552182
rajsite <rajsite@users.noreply.github.com>
2020-06-12 00:36:28 -0500

    DebugLog node implementation

---
+++ DebugLog/DebugLog.gcomp/Support/DebugLog.js
@@ -0,0 +1,49 @@
+(function () {
+    'use strict';
+
+    const debugLog = function (prefix) {
+        // Find web application
+        const webAppElements = document.querySelectorAll('ni-web-application');
+        if (webAppElements.length !== 1) {
+            console.log('Expected a single ni-web-application element in page for debugging.');
+            return;
+        }
+        const [webAppElement] = webAppElements;
+
+        const isSupportedNXG = webAppElement && webAppElement.vireoInstance && webAppElement.vireoHelpers;
+        if (!isSupportedNXG) {
+            console.log('WebVIDebugger not supported in this version of NXG.');
+            return;
+        }
+
+        // Find vireo instance
+        const vireo = webAppElement.vireoInstance;
+        const vireoHelpers = webAppElement.vireoHelpers;
+
+        // Read value from dataspace
+        const viName = 'DebugLog::Debug Log.gvi';
+        const viNameEncoded = vireoHelpers.staticHelpers.encodeIdentifier(viName);
+        const valueRef = vireo.eggShell.findValueRef(viNameEncoded, 'dataItem_Valueout');
+        const variantValueJSON = vireo.eggShell.readJSON(valueRef);
+        const variantValue = JSON.parse(variantValueJSON);
+        const variantDataName = '_data';
+        const value = variantValue[variantDataName];
+
+        // log value
+        const isInBrowser = webAppElement.location === 'BROWSER';
+        if (isInBrowser) {
+            if (prefix === '') {
+                console.log(value);
+            } else {
+                console.log(prefix, value);
+            }
+        } else {
+            // The editor console output only shows one line of the output
+            // so concatenate the message to a single line
+            const valueJSON = JSON.stringify(value);
+            console.log(`${prefix}${valueJSON}`);
+        }
+    };
+
+    window.WebVIDebugLog = {debugLog};
+}());
