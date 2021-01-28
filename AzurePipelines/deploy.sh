859ecbb37e8479087127f42ef29c71e9e38109c3
Milan Raj <rajsite@users.noreply.github.com>
2021-01-28 11:10:05 -0600

    AzurePipelines explicitly install dpl-pages

--- AzurePipelines/deploy.sh
+++ AzurePipelines/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gem install dpl -v 1.10.15
+gem install dpl dpl-pages
 dpl --provider=pages --skip_cleanup --name=rajsite --email=rajsite@users.noreply.github.com --github-token=$GHPAGESTOKEN
