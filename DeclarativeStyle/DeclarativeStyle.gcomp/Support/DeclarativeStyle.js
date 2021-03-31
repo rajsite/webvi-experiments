23e40ca2e49d94c09e3b59798143453be0bda5b2
rajsite <rajsite@users.noreply.github.com>
2021-03-31 02:08:03 -0500

    DeclarativeStyle api restructure

--- DeclarativeStyle/DeclarativeStyle.gcomp/Support/DeclarativeStyle.js
+++ DeclarativeStyle/DeclarativeStyle.gcomp/Support/DeclarativeStyle.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
-    const dataGridStringColumnValueBasedBackgroundColor = async function (selector, index, text, color) {
+    const dataGridStringColumnTextMatch = function (config) {
+        // todo support operator and caseInsensitive
+        const {selector, index, text} = config;
         const elements = document.querySelectorAll(selector);
         if (elements.length !== 1) {
             throw new Error(`Expected a single element with selector (${selector}) but found: ${elements.length}`);
@@ -14,12 +16,26 @@
         if (index < 0 || index >= fieldLength) {
             throw new Error(`Expected column index to be in range [0-${fieldLength})`);
         }
+        // todo validate column type
         // NXG columns are zero-indexed, css nth-child is one-indexed, the first column is for row header (even when not visible)
         const colIndex = index + 2;
-        const rule = `${selector} div[role="row"] > div[role="gridcell"]:nth-child(${colIndex}) ni-string-control[text="${text}"] {
-            background-color: ${color};
+        const rule = `${selector} div[role="row"] > div[role="gridcell"]:nth-child(${colIndex}) ni-string-control[text="${text}"]`;
+        return rule;
+    };
+
+    const types = {
+        'datagrid-stringcolumn-textmatch': dataGridStringColumnTextMatch
+    };
+
+    const createStyle = function (ruleSelectorJSON, rulePropertiesJSON) {
+        const {type, configJSON} = JSON.parse(ruleSelectorJSON);
+        const config = JSON.parse(configJSON);
+        const selector = types[type](config);
+        const ruleProperties = JSON.parse(rulePropertiesJSON);
+        const properties = ruleProperties.map(ruleProperty => `${ruleProperty.name}: ${ruleProperty.value};`).join('\n');
+        const rule = `${selector} {
+            ${properties}
         }`;
-        window.myrule = rule;
         const style = document.createElement('style');
         document.head.insertAdjacentElement('beforeend', style);
         style.sheet.insertRule(rule);
@@ -30,5 +46,5 @@
         style.parentNode.removeChild(style);
     };
 
-    window.WebVIDeclarativeStyle = {dataGridStringColumnValueBasedBackgroundColor, removeStyle};
+    window.WebVIDeclarativeStyle = {createStyle, removeStyle};
 }());
