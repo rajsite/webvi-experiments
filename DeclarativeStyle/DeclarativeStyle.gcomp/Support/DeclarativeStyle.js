e5b41c0628eddc39ff5a83624c3fa9714b93cc71
rajsite <rajsite@users.noreply.github.com>
2021-04-12 20:47:04 -0500

    DeclarativeStyle add select string col and horizontal scroll

--- DeclarativeStyle/DeclarativeStyle.gcomp/Support/DeclarativeStyle.js
+++ DeclarativeStyle/DeclarativeStyle.gcomp/Support/DeclarativeStyle.js
@@ -10,7 +10,7 @@
 
     const getMatcher = function (operator) {
         const matcher = matchers[operator];
-        // LabVIEW will clamp matchers so unlikely this case will be hit
+        // LabVIEW will clamp enum values so unlikely this case will be hit
         if (matcher === undefined) {
             throw new Error(`Unexpected operator for ${operator}. Supported operators are ${Object.keys(matchers).join(',')}.`);
         }
@@ -36,33 +36,47 @@
         return element;
     };
 
-    const dataGridStringColumnTextMatch = function (config) {
-        const {selector, index, operator, text} = config;
-        const element = findControl(selector, 'NI-DATA-GRID');
-
-        // The attribute named text is always case insensitive: https://html.spec.whatwg.org/multipage/semantics-other.html#case-sensitivity-of-selectors
-        // Can support case sensitive matching when selector is available: https://caniuse.com/mdn-css_selectors_attribute_case_sensitive_modifier
-        const attributeSelector = createAttributeSelector('text', operator, text, true);
-
+    const createDataGridStringColumnSelector = function (element, index) {
         const niType = JSON.parse(element.niType);
         const fieldLength = niType.subtype.fields.length;
         if (index < 0 || index >= fieldLength) {
-            throw new Error(`Expected column index ${index} to be in range [0-${fieldLength - 1}] for data grids with selector ${selector}.`);
+            throw new Error(`Expected column index ${index} to be in range [0-${fieldLength - 1}] for data grid.`);
         }
+        // Check against the template element in case there are no values at runtime
         const column = element.querySelectorAll(`ni-data-grid-column[index="${index}"] > ni-string-control`);
         if (column.length !== 1) {
-            throw new Error(`String column not found at column index ${index} for data grid with selector ${selector}.`);
+            throw new Error(`String column not found at column index ${index} for data grid.`);
         }
 
         // +1 as NXG is zero-indexed while css nth-child is one-indexed
         // +1 as the first column is for row header (even when not visible)
         const colIndex = index + 2;
-        const rule = `${selector} div[role="row"] > div[role="gridcell"]:nth-child(${colIndex}) ni-string-control${attributeSelector}`;
+        return `div[role="row"] > div[role="gridcell"]:nth-child(${colIndex}) ni-string-control`;
+    };
+
+    const dataGridStringColumnTextMatch = function (config) {
+        const {selector, index, operator, text} = config;
+        const element = findControl(selector, 'NI-DATA-GRID');
+
+        // The attribute named `text` is always case insensitive: https://html.spec.whatwg.org/multipage/semantics-other.html#case-sensitivity-of-selectors
+        // Can support case sensitive matching when selector is available: https://caniuse.com/mdn-css_selectors_attribute_case_sensitive_modifier
+        const attributeSelector = createAttributeSelector('text', operator, text, true);
+        const dataGridStringColumnSelector = createDataGridStringColumnSelector(element, index);
+        const rule = `${selector} ${dataGridStringColumnSelector}${attributeSelector}`;
+        return rule;
+    };
+
+    const dataGridStringColumn = function (config) {
+        const {selector, index} = config;
+        const element = findControl(selector, 'NI-DATA-GRID');
+        const dataGridStringColumnSelector = createDataGridStringColumnSelector(element, index);
+        const rule = `${selector} ${dataGridStringColumnSelector}`;
         return rule;
     };
 
     const types = {
-        'datagrid-stringcolumn-textmatch': dataGridStringColumnTextMatch
+        'datagrid-stringcolumn-textmatch': dataGridStringColumnTextMatch,
+        'datagrid-stringcolumn': dataGridStringColumn
     };
 
     const createStyle = function (ruleSelectorJSON, rulePropertiesJSON) {
