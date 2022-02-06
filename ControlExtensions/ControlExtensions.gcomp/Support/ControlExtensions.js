e802f3d4899d9dcefcf4ec20d1526a4e8778531c
rajsite <rajsite@users.noreply.github.com>
2022-02-06 16:52:54 -0600

    ControlExtensions cache dataitem reference

--- ControlExtensions/ControlExtensions.gcomp/Support/ControlExtensions.js
+++ ControlExtensions/ControlExtensions.gcomp/Support/ControlExtensions.js
@@ -1,28 +1,55 @@
 (function () {
     'use strict';
 
+    class DataItem {
+        constructor (viName, dataItemPath) {
+            this._viName = viName;
+            this._dataItemPath = dataItemPath;
+            this._vireo = undefined;
+            this._vireoHelpers = undefined;
+            this._valueRef = undefined;
+        }
+
+        getValueRef () {
+            if (this._valueRef === undefined) {
+                // Find web application
+                const webAppElements = document.querySelectorAll('ni-web-application');
+                if (webAppElements.length !== 1) {
+                    throw new Error('Expected a single ni-web-application element in page.');
+                }
+                const [webAppElement] = webAppElements;
+
+                const isSupported = webAppElement && webAppElement.vireoInstance && webAppElement.vireoHelpers;
+                if (!isSupported) {
+                    throw new Error('Not supported in this version of G Web Development Software.');
+                }
+
+                // Find vireo instance
+                const vireo = webAppElement.vireoInstance;
+                const vireoHelpers = webAppElement.vireoHelpers;
+
+                // Read value from dataspace
+                const viNameEncoded = vireoHelpers.staticHelpers.encodeIdentifier(this._viName);
+                const valueRef = vireo.eggShell.findValueRef(viNameEncoded, this._dataItemPath);
+                this._vireo = vireo;
+                this._vireoHelpers = vireoHelpers;
+                this._valueRef = valueRef;
+            }
+            return this._valueRef;
+        }
+    }
+
+    class ReferenceDataItem extends DataItem {
+        getValue () {
+            const valueRef = this.getValueRef();
+            return this._vireo.eggShell.readJavaScriptRefNum(valueRef);
+        }
+    }
+
+    const obtainJavaScriptReferenceGObjectDataItem = new ReferenceDataItem('ControlExtensions::Support::Obtain JavaScript Reference - GObject.gvi', 'dataItem_Referenceprobe');
+
     const obtainJavaScriptReferenceGObject = function () {
-        // Find web application
-        const webAppElements = document.querySelectorAll('ni-web-application');
-        if (webAppElements.length !== 1) {
-            throw new Error('Expected a single ni-web-application element in page.');
-        }
-        const [webAppElement] = webAppElements;
-
-        const isSupported = webAppElement && webAppElement.vireoInstance && webAppElement.vireoHelpers;
-        if (!isSupported) {
-            throw new Error('ControlExtensions not supported in this version of G Web Development Software.');
-        }
-
-        // Find vireo instance
-        const vireo = webAppElement.vireoInstance;
-        const vireoHelpers = webAppElement.vireoHelpers;
-
-        // Read value from dataspace
-        const viName = 'ControlExtensions::Support::Obtain JavaScript Reference - GObject.gvi';
-        const viNameEncoded = vireoHelpers.staticHelpers.encodeIdentifier(viName);
-        const valueRef = vireo.eggShell.findValueRef(viNameEncoded, 'dataItem_Referenceprobe');
-        const value = vireo.eggShell.readJavaScriptRefNum(valueRef);
+        const value = obtainJavaScriptReferenceGObjectDataItem.getValue();
         if ((value && value.element instanceof HTMLElement) === false) {
             throw new Error('Expected reference to be an HTML control');
         }
