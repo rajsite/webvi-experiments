699a48bbae2e25421563b27ae67efe303cffb87e
rajsite <rajsite@users.noreply.github.com>
2022-03-27 16:21:11 -0500

    ControlExtensions add APIs from ControlTools

--- ControlExtensions/ControlExtensions.gcomp/Support/ControlExtensions.js
+++ ControlExtensions/ControlExtensions.gcomp/Support/ControlExtensions.js
@@ -1,6 +1,126 @@
 (function () {
     'use strict';
 
+    // Shared
+    const findAncestorByTagname = function (element, tagName) {
+        let selectedElement;
+        for (let currentElement = element; currentElement !== null; currentElement = currentElement.parentElement) {
+            if (currentElement.tagName === tagName) {
+                selectedElement = currentElement;
+                break;
+            }
+        }
+        return selectedElement;
+    };
+
+    const validateControl = function (element, tagName) {
+        if (element instanceof HTMLElement === false) {
+            throw new Error('Expected reference to be an HTML control');
+        }
+
+        if (element.tagName !== tagName) {
+            throw new Error(`Expected reference to target a ${tagName} but instead found ${element.tagName}`);
+        }
+    };
+
+    // Data Grid
+    const dataGridValidateAllowsSorting = function (element) {
+        if (element.allowSorting === false) {
+            throw new Error('DataGrid must be an indicator and the "Sorted view" option must be enabled in editor under Properties >> Behavior >> Sorted view to programmatically change the sort order.');
+        }
+    };
+
+    const validateDataGridColumnIndex = function (element, indexString) {
+        const datafields = element.jqref.jqxGrid('source')._source.datafields;
+        if (!datafields) {
+            throw new Error('A value must be set on the Data Grid before manipulating a column.');
+        }
+        const fieldNames = element.jqref.jqxGrid('source')._source.datafields.map(field => field.name);
+        if (fieldNames.includes(indexString) === false) {
+            throw new Error(`Data Grid column index ${indexString} is not a valid column index.`);
+        }
+    };
+
+    const dataGridColumnSorting = function (element, index, sort) {
+        validateControl(element, 'NI-DATA-GRID');
+        dataGridValidateAllowsSorting(element);
+        const indexString = String(index);
+        validateDataGridColumnIndex(element, indexString);
+
+        // From: https://www.jqwidgets.com/jquery-widgets-documentation/documentation/jqxgrid/jquery-grid-sorting.htm
+        if (sort === 'ascending') {
+            element.jqref.jqxGrid('sortby', indexString, 'asc');
+        } else if (sort === 'descending') {
+            element.jqref.jqxGrid('sortby', indexString, 'desc');
+        } else if (sort === 'none') {
+            element.jqref.jqxGrid('removesort');
+        } else {
+            throw new Error(`Unexpected Data Grid sort value ${sort}`);
+        }
+    };
+
+    // Listbox
+    const listboxItemTooltipHandler = Symbol('Mouse handler for tooltips if enabled');
+    const listboxItemTooltip = function (element) {
+        validateControl(element, 'JQX-LIST-BOX');
+        if (element[listboxItemTooltipHandler]) {
+            return;
+        }
+        element[listboxItemTooltipHandler] = event => {
+            const listItem = findAncestorByTagname(event.target, 'JQX-LIST-ITEM');
+            if (listItem !== undefined) {
+                const labelElements = listItem.querySelectorAll('.jqx-content-label');
+                if (labelElements.length === 1) {
+                    const [labelElement] = labelElements;
+                    const title = labelElement.textContent;
+                    listItem.title = title;
+                }
+            }
+        };
+        element.addEventListener('mouseover', element[listboxItemTooltipHandler]);
+    };
+
+    // Tab
+    const tabSelectorVisible = function (element, visible) {
+        validateControl(element, 'NI-TAB-CONTROL');
+        element.tabSelectorHidden = !visible;
+    };
+
+    // Tree
+    const treeValidateColumnIndex = function (element, index) {
+        const lastIndex = element.columns.length - 1;
+        if (index > lastIndex) {
+            throw new Error(`Attempted to use column at index ${index} but valid indices are from 0-${lastIndex}`);
+        }
+    };
+
+    const treeCellTooltipHandler = Symbol('Mouse handler for tooltips if enabled');
+    const treeCellTooltip = function (element) {
+        validateControl(element, 'NI-TREE-GRID');
+        if (element[treeCellTooltipHandler]) {
+            return;
+        }
+        element[treeCellTooltipHandler] = event => {
+            const gridCell = findAncestorByTagname(event.target, 'JQX-GRID-CELL');
+            if (gridCell !== undefined) {
+                const labelElements = gridCell.querySelectorAll('.jqx-label');
+                if (labelElements.length === 1) {
+                    const [labelElement] = labelElements;
+                    const title = labelElement.textContent;
+                    gridCell.title = title;
+                }
+            }
+        };
+        element.addEventListener('mouseover', element[treeCellTooltipHandler]);
+    };
+
+    const treeColumnWidth = function (element, index, width) {
+        validateControl(element, 'NI-TREE-GRID');
+        treeValidateColumnIndex(element, index);
+        element.columns[index].width = width;
+    };
+
+    // Obtain JavaScript Reference
     class DataItem {
         constructor (viName, dataItemPath) {
             this._viName = viName;
@@ -56,23 +176,12 @@
         return value.element;
     };
 
-    const validateControl = function (element, tagName) {
-        if (element instanceof HTMLElement === false) {
-            throw new Error('Expected reference to be an HTML control');
-        }
-
-        if (element.tagName !== tagName) {
-            throw new Error(`Expected reference to target a ${tagName} but instead found ${element.tagName}`);
-        }
-    };
-
-    const tabSelectorVisible = function (element, visible) {
-        validateControl(element, 'NI-TAB-CONTROL');
-        element.tabSelectorHidden = !visible;
-    };
-
     window.WebVIControlExtensions = {
-        obtainJavaScriptReferenceGObject,
-        tabSelectorVisible
+        dataGridColumnSorting,
+        listboxItemTooltip,
+        tabSelectorVisible,
+        treeCellTooltip,
+        treeColumnWidth,
+        obtainJavaScriptReferenceGObject
     };
 }());
---
+++ ControlExtensions/ControlExtensions.gcomp/Support/ControlExtensionsPanelBackground.js
@@ -0,0 +1,59 @@
+(function () {
+    'use strict';
+
+    const performPanelBackgroundExtend = () => {
+        const webAppElements = document.querySelectorAll('ni-web-application');
+        if (webAppElements.length !== 1) {
+            throw new Error(`Expected a single ni-web-application element, found: ${webAppElements.length}`);
+        }
+        const [webAppElement] = webAppElements;
+        // Runs before the custom element has initialized so read the HTML value
+        const isInBrowser = webAppElement.getAttribute('location') === 'BROWSER';
+        // When running in the editor the body background has margins outside the panel that
+        // is controlled by the editor so we skip extending the background
+        if (isInBrowser === false) {
+            return;
+        }
+        // When running in browser set a class on body to adjust styles
+        document.body.classList.add('webvi-control-tools-background');
+        const frontPanels = document.querySelectorAll('ni-front-panel');
+        if (frontPanels.length !== 1) {
+            throw new Error(`Expected a single ni-front-panel to query background color, found: ${frontPanels.length}`);
+        }
+        const [frontPanel] = frontPanels;
+        const background = window.getComputedStyle(frontPanel).getPropertyValue('--ni-background');
+        if (background === '') {
+            throw new Error('The --ni-background css property is not defined for ni-front-panel');
+        }
+        document.body.style.setProperty('--webvi-control-tools-background', background);
+    };
+
+    const ready = function (callback) {
+        if (document.readyState === 'loading') {
+            document.addEventListener('readystatechange', function readyStateChangeHandler () {
+                document.removeEventListener('readystatechange', readyStateChangeHandler);
+                callback();
+            });
+        } else {
+            callback();
+        }
+    };
+
+    let loadException;
+
+    const checkPanelBackgroundExtend = () => {
+        if (loadException !== undefined) {
+            throw loadException;
+        }
+    };
+
+    ready(() => {
+        try {
+            performPanelBackgroundExtend();
+        } catch (ex) {
+            loadException = ex;
+        }
+    });
+
+    window.WebVIControlExtensionsPanelBackground = {checkPanelBackgroundExtend};
+}());
