5f25e84c4ad930b3a6b35a5cf2d4035f773d8506
rajsite <rajsite@users.noreply.github.com>
2022-06-26 17:11:29 -0500

    File error when triggering download in editor

--- File/File.gcomp/Support/File.js
+++ File/File.gcomp/Support/File.js
@@ -1,6 +1,27 @@
 (function () {
     'use strict';
 
+    let isInBrowser;
+    const validateCanDownload = function () {
+        if (isInBrowser === undefined) {
+            const webAppElements = document.querySelectorAll('ni-web-application');
+            if (webAppElements.length !== 1) {
+                console.log('Expected a single ni-web-application element in page for debugging.');
+                return;
+            }
+            const [webAppElement] = webAppElements;
+            isInBrowser = webAppElement.location === 'BROWSER';
+        }
+        // Note: This check may need to be revisted if the editor supports this in the future
+        // it currently just assumes the editor cannot download a file
+        if (!isInBrowser) {
+            throw new Error('Downloading a file can only performed when using Run in Browser or in a deployed web application. A file download cannot be triggered when running in the editor.');
+        }
+        if ('download' in HTMLAnchorElement.prototype === false) {
+            throw new Error('Browser does not support anchor download attribute, unable to trigger download');
+        }
+    };
+
     const validateFile = function (reference) {
         if (reference instanceof File === false) {
             throw new Error('Reference is not a valid File');
@@ -21,9 +42,7 @@
 
     const downloadFile = function (file, overriddenFileName) {
         validateFile(file);
-        if ('download' in HTMLAnchorElement.prototype === false) {
-            throw new Error('Browser does not support anchor download attribute, unable to trigger download');
-        }
+        validateCanDownload();
         const url = URL.createObjectURL(file);
         const downloadElement = document.createElement('a');
         document.body.appendChild(downloadElement);
