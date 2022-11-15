657c1e2609b5bb98fe53155ba17ef17eca2719fb
rajsite <rajsite@users.noreply.github.com>
2022-11-14 23:15:15 -0600

    WebVIDeno main.ts proof of concept

--- WebVIDeno/WebVIDeno.gcomp/main.ts
+++ WebVIDeno/WebVIDeno.gcomp/main.ts
@@ -2,9 +2,9 @@ import "https://deno.land/x/xhr@0.1.0/mod.ts";
 
 import vireoHelpers from './Support/node_modules/vireo/source/core/vireo.loader.wasm32-unknown-emscripten.release.js';
 
-
+const url = new URL("./Support/node_modules/vireo/dist/wasm32-unknown-emscripten/release/vireo.core.wasm", import.meta.url);
 var vireo = await vireoHelpers.createInstance({
-    wasmUrl: 'file://./Support/node_modules/vireo/dist/wasm32-unknown-emscripten/release/vireo.core.wasm'
+    wasmUrl: url.href
 });
 
 var viaCode = `start(VirtualInstrument<(clump(Println("Hello, sky. I can fly.")))>)`;
