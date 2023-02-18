e219d8a2f5acd4b618bf5d0cde91a5a96aba3fcc
rajsite <rajsite@users.noreply.github.com>
2023-02-18 02:01:35 -0600

    WebVIDeno serve static files

--- WebVIDeno/WebVIDeno.gcomp/Support/src/main.ts
+++ WebVIDeno/WebVIDeno.gcomp/Support/src/main.ts
@@ -1,6 +1,12 @@
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.177.0/http/file_server.ts";
 import { VireoDeno } from "./vireo-deno.ts";
 const vireo = await VireoDeno.createInstance();
 const viaCode = `start(VirtualInstrument<(clump(Println("Hello, sky. I can fly.")))>)`;
 vireo.eggShell.loadVia(viaCode);
 await vireo.eggShell.executeSlicesUntilClumpsFinished();
 console.log('finished :D');
+
+serve(async (req) => {
+    return await serveDir(req);
+});
