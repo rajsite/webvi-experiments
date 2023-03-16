9206e7d41451bd3178ed889f7e7b3697b9e9dffd
rajsite <rajsite@users.noreply.github.com>
2023-03-16 16:29:55 -0500

    Deno tweak static file generation

--- Deno/webvi-deno/build/create-static-runner.ts
+++ Deno/webvi-deno/build/create-static-runner.ts
@@ -9,16 +9,19 @@ const staticRunnerUrl = new URL('index.ts', htmlURL);
 const vireoCodeUrl = new URL(extractedUrls.vireoSource, htmlURL);
 const vireoCode = await Deno.readTextFile(vireoCodeUrl);
 const formattedScriptSources = extractedUrls.scriptSources
-    .map(url => `import '${url.startsWith('.') ? '' : './'}${url}';`)
+    .map(url => url.startsWith('.') ? url : `./${url}`)
+    .map(url => `import '${url}';`)
     .join('\n');
 const base64 = btoa(vireoCode);
 const staticFile = `
 ${formattedScriptSources}
 import {run} from 'https://raw.githubusercontent.com/rajsite/webvi-experiments/ea259f96644f0285775642784206a4b38bbb170a/Deno/webvi-deno/run/runner.ts';
+const viaCode = getViaCode();
+await run(viaCode);
 
-const vireoCode64 = '${base64}';
-const vireoCode = atob(vireoCode64);
-await run(vireoCode);
+function getViaCode() {
+    return atob('${base64}');
+}
 `;
 
 await Deno.writeTextFile(staticRunnerUrl, staticFile);
--- Deno/webvi-deno/static-dev.ts
+++ Deno/webvi-deno/static-dev.ts
@@ -2,7 +2,4 @@ import '../Builds/WebApp_Default Web Server/URL/Support/URL.js';
 import '../Builds/WebApp_Default Web Server/DenoHTTP/Support/DenoHTTP.js';
 import { run } from './run/runner.ts';
 const viaCode = await Deno.readTextFile(new URL('../Builds/WebApp_Default Web Server/index.via.txt', import.meta.url));
-
-const base64 = btoa(viaCode);
-const data = atob(base64);
-await run(data);
+await run(viaCode);
