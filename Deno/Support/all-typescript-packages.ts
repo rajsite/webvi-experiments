72e1b3e32faea622335eee24a1919ab9d76f2825
rajsite <rajsite@users.noreply.github.com>
2024-07-24 13:29:35 -0500

    Deno tasks refactor

--- Deno/Support/all-typescript-packages.ts
+++ Deno/Support/all-typescript-packages.ts
@@ -1,7 +1,7 @@
 // Package to reference all modules with deno imports
 // to use with vendoring / lock file creation
 
-import './make-main.ts';
+import './convert-html.ts';
 import './make-vireo-dataurl.ts';
 import '../Library/Support/Deno/deno.ts';
 import '../Library/Support/DenoHTTP/deno-http.ts';
