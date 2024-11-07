d949a5588ba8db6e5cbc6b77e831c150939f56e3
Milan Raj <rajsite@users.noreply.github.com>
2024-11-07 10:33:58 -0600

    Test not installing recommended packages

--- BuildTools/install.ps1
+++ BuildTools/install.ps1
@@ -49,7 +49,7 @@ if ($install_editor)
 
     Write-Host "Installing Editor..."
     Invoke-PrintDiskspace
-    Invoke-Run $nipm 'install ni-g-web-development --accept-eulas --assume-yes --include-recommended'
+    Invoke-Run $nipm 'install ni-g-web-development --accept-eulas --assume-yes'
     $time = (Get-Date).ToUniversalTime()
     Write-Host "...done at UTC $time"
     Invoke-PrintDiskspace
